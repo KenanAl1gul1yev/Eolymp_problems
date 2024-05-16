a0596f50f718a6efdc5d91714b6aa60981ed108f
Kenan Eminli <kenaneliquliyev05042020@gmail.com>
2024-05-17 00:01:12 +0400

    Number of digits

---
+++ 8983/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int sum = 0, i = 0, num;
+    char operand;
+    while (1){
+        if (i % 2 == 0) scanf("%d", &num);
+        else{
+
+        }
+        i += 1;
+    }
+    return 0;
+}
