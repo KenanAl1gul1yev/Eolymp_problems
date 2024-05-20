f381d5244bf7bf8c5f8a45d7eb8b08e9dc4b982d
Kenan Eminli <kenaneliquliyev05042020@gmail.com>
2024-05-21 00:11:28 +0400

    Print array

---
+++ 8953/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n; i++){
+        printf("%d\n", arr[i]);
+    }
+    return 0;
+}
