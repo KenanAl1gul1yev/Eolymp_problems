0b3bb951323f8e7698532be0728089d86c4656c6
Kenan Eminli <kenaneliquliyev05042020@gmail.com>
2024-05-23 23:51:35 +0400

    Minimum among the larger

---
+++ 8900/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = n + 1;  ; i ++ ){
+        if (i % 7 == 0){
+            printf("%d", i);
+            return 0;
+        }
+    }
+
+    return 0;
+}
