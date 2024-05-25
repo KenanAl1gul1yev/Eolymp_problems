e15addf437373b48f0b70ac5fbbcfb0b27383b18
Kenan Eminli <kenaneliquliyev05042020@gmail.com>
2024-05-26 02:15:05 +0400

    One-digit number

---
+++ 8873/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    if (n >= - 9 && n <= 9){
+        printf("Ok");
+    }
+    else{
+        printf("No");
+    }
+    return 0;
+}
