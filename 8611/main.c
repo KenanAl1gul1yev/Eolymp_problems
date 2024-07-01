e3480b2bf6fbaa4d382ba71249f1c8e3b91351fe
Kenan Eminli <kenaneliquliyev05042020@gmail.com>
2024-07-02 01:58:07 +0400

    Water and Ice

---
+++ 8611/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    scanf ("%d", &n);
+    if (n > 0){
+        printf("Water");
+    }
+    else{
+        printf("Ice");
+    }
+    return 0;
+}
