0299362b97f7eefb6a8e2668d67bd57cf2566a87
Kenan Eminli <kenaneliquliyev05042020@gmail.com>
2024-07-22 21:17:39 +0400

    Multiplication table

---
+++ 8545/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i ++){
+        for (int j = 1; j <= n; j++){
+            printf("%d ", i * j);
+            if (i * (j + 1) < 10) printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
