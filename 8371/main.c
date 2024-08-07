334cc5e7c790cc5775d72a963cfd7e64f217d7c9
Kenan Eminli <kenaneliquliyev05042020@gmail.com>
2024-08-07 12:30:44 +0400

    Even or Odd

---
+++ 8371/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    if (n % 2 == 1){
+        printf("ODD");
+    }
+    else{
+        printf("EVEN");
+    }
+    return 0;
+}
