e80ae33b63d4870be459d2e172fe3245e56199e7
Kenan Eminli <kenaneliquliyev05042020@gmail.com>
2024-08-29 15:40:10 +0400

    Odd elements

---
+++ 7841/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, x;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++){
+        scanf("%d", &x);
+        if (x % 2 != 0){
+            printf("%d ", x);
+        }
+    }
+
+     return 0;
+}
